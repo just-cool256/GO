1352edb487c016e7fc48da44f292a2245f788a5d
just-cool256 <bhartisomra34@gmail.com>
2026-02-07 16:03:06 +0530

    Diving Deeper into Slices

--- Arrays,Slices&Maps/Arrays/lists.go
+++ Arrays,Slices&Maps/Arrays/lists.go
@@ -17,15 +17,60 @@ func main() {
 	featuredPrices = prices[:3]
 	fmt.Println(featuredPrices)
 
-	featuredPrices = prices[1:]
-	fmt.Println(featuredPrices)
+	fmt.Println()
 
-	featuredPrices = prices[:]
+	fmt.Println("senerio - 1")
+	fmt.Println("featuredPrices = prices[1:3]")
+	featuredPrices = prices[1:3]
+	// Slices are like a reference, like a window into an array. When we created an array like prices that array is stored in memory.
+	// When we then create a slice based on that array, we get a window into that array.
+	// Therefore if you would modify an element in a slice, we would also modify the same element in the original array.
+	featuredPrices[0] = 199.99
 	fmt.Println(featuredPrices)
+	// fmt.Println(prices)
+	// Important Implication: When you create a slice, you don't copy the original array, so you don't have that copy in memory that occupies extra memory space.
+	// Instead you only have 1 array in memory and your slice is just a tiny reference to a part of that array.
+	// Hence it's a very memory efficient way of selecting parts of an array or editing parts of an array.
 
-	featuredPrices = prices[:3][1:]
+	// Go also saves some metadata for our slices that can be useful to look into.
+	// For every slice we got a length and a capacity
+
+	fmt.Println(len(featuredPrices), cap(featuredPrices)) //len - 2, cap - 3
+	// length - no. of items in a slice or array
+	// cap - tells us - Go memorized that there is more content available to the right of the selected slice.
+	// we can reslice to pick up more elements from right but that is not possible from left
+
+	// fmt.Println()
+	// fmt.Println("featuredPrices = featuredPrices[1:]")
+	// featuredPrices = featuredPrices[1:]
+	// fmt.Println(featuredPrices)
+	// fmt.Println(len(featuredPrices), cap(featuredPrices))
+
+	fmt.Println()
+	fmt.Println("featuredPrices = featuredPrices[:3]")
+	featuredPrices = featuredPrices[:3] //- gave panic error
 	fmt.Println(featuredPrices)
+	fmt.Println(len(featuredPrices), cap(featuredPrices))
+
+	fmt.Println("senerio-2")
+	featuredPrices = prices[1:]             //{199.99, 45.99, 20.0}
+	highlightedPrices := featuredPrices[:1] //{199.99}
+	fmt.Println(highlightedPrices)
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+	highlightedPrices = highlightedPrices[:3]
+	fmt.Println(highlightedPrices) // {199.99 45.99 20}
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+	// featuredPrices = prices[:]
+	// fmt.Println(featuredPrices)
+
+	// featuredPrices = prices[:3][1:]
+	// fmt.Println(featuredPrices)
 
 	// featuredPrices = prices[-1:] - can't work with negative indicies
 	// featuredPrices = prices[1:7] - can't pick higher end index than the original array has
 }
+
+// When we suddenly select more than was previously stored in a slice - that works
+// Bcz if you select more to the end of an array, you can do that bcz go memorizes that, but it only work for more values to the right of the existing slice
