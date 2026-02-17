4467d25a1b1e9ed6a5a7d408277e75f8173693e8
just-cool256 <bhartisomra34@gmail.com>
2026-02-17 21:56:25 +0530

    Maps vs Structs

--- Arrays,Slices&Maps/Maps/maps.go
+++ Arrays,Slices&Maps/Maps/maps.go
@@ -18,3 +18,11 @@ func main() {
 	delete(website, "Google")
 	fmt.Println(website) // map[Amazon Web Services:https://aws.amazon.com Facebook:https://www.facebook.com]
 }
+
+/*
+difference between map and struct
+1. For maps, you can use anything as a key like string, int, float or even some array or a struct as a key
+2. With structs, you have predefined data structures, we can't add or delete fields afterwards
+You don't use structs to manage multiple values of the same kind with different keys, but instead you use structs to describe data entities in your programs.
+And a map on the other hand is used if you have a collection of values, which probably describe the same thing, though technically, not necessarily, but then you can assign your own labels, your own keys, and use any kinds of values for those keys, that make sense to you
+*/
