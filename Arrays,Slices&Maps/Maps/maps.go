4f9be3d1d91d084efe9fe41f8310c132f3e04583
just-cool256 <bhartisomra34@gmail.com>
2026-02-17 21:35:14 +0530

    Mutating Maps

--- Arrays,Slices&Maps/Maps/maps.go
+++ Arrays,Slices&Maps/Maps/maps.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// Map is always dynamic
 	website := map[string]string{
 		"Google":              "https://www.google.com",
 		"Amazon Web Services": "https://aws.amazon.com",
 	}
 
-	fmt.Println(website) // map[Amazon Web Services:https://aws.amazon.com Google:https://www.google.com]
+	fmt.Println(website)           // map[Amazon Web Services:https://aws.amazon.com Google:https://www.google.com]
+	fmt.Println(website["Google"]) // https://www.google.com
+
+	website["Facebook"] = "https://www.facebook.com"
+	fmt.Println(website) // map[Amazon Web Services:https://aws.amazon.com Facebook:https://www.facebook.com Google:https://www.google.com]
+
+	delete(website, "Google")
+	fmt.Println(website) // map[Amazon Web Services:https://aws.amazon.com Facebook:https://www.facebook.com]
 }
