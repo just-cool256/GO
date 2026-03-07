305845628f12b9c901433c2e7fe4258e2013a9a6
just-cool256 <bhartisomra34@gmail.com>
2026-03-07 12:33:29 +0530

    Returning Functions as Values

--- FunctionsDeepDive/FunctionValue&FunctionTypes/main.go
+++ FunctionsDeepDive/FunctionValue&FunctionTypes/main.go
@@ -4,18 +4,29 @@ import (
 	"fmt"
 )
 
+// Function types
 type transformFn func(int) int
 
 // type anotherFn func(int, []string, map[string][]int) ([]int, string)
 
 // Functions are first class values in go
 // We can use functions themselves as parameter values for other functions
+// Function can also return functions as return values
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 6, 7, 8}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformFn1 := getTransformerFunction(&numbers)
+	transformFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers1 := transformNumbers(&numbers, transformFn1)
+	transformedNumbers2 := transformNumbers(&moreNumbers, transformFn2)
+	fmt.Println(transformedNumbers1)
+	fmt.Println(transformedNumbers2)
 
 }
 
@@ -25,6 +36,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers[i] = transform(val)
 	}
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
